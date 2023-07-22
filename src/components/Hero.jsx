89bb9933c7ef0dc77c5288f3d4000b4e8340fb04
g-sillva <silvagabriel.limars@gmail.com>
2023-07-22 18:37:28 -0300

    Finished Hero page component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <h1 className="nav_text">AI Summarizer</h1>
 
-export default Hero
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/g-sillva/AI-Summarizer")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc pt-3">
+        Experience simplified reading using <span className="font-bold">AI Summarizer</span>, an open-source article
+        summarizer designed to condense lengthy articles into clear and concise
+        information.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
