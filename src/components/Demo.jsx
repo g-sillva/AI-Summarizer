ec7c4615416e94919a561d9d74323b41cf4257b8
g-sillva <silvagabriel.limars@gmail.com>
2023-07-23 15:50:41 -0300

    Implemented article summary view

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,11 +8,7 @@ const Demo = () => {
     url: "",
     summary: "",
   });
-  const [allArticles, setAllArticles] = useState([
-    { url: "http://localhost:1234/", summary: "lorem ipsum dolor asit met" },
-    { url: "http://localhost:5678/", summary: "lorem ipsum dolor asit met" },
-    { url: "http://localhost:9999/", summary: "lorem ipsum dolor asit met" },
-  ]);
+  const [allArticles, setAllArticles] = useState([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -93,6 +89,31 @@ const Demo = () => {
       </div>
 
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Something went wrong, try again <br />{" "}
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="orange_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
